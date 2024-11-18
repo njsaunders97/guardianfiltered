97fdd90383ed84fba1fecd9fea90e588dbbd1db8
Nick Saunders <njsaunders97@gmail.com>
2024-11-18 15:12:08 +0100

    Flex tests

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='flex'>
       <NavBar />
-      <Feed />
+      {/* <Feed /> */}
     </div>
   );
 }
--- src/features/NavBar.js
+++ src/features/NavBar.js
@@ -3,15 +3,17 @@ import { FaFilter } from 'react-icons/fa';
 
 function NavBar() {
     return (
-        <div className="fixed top-0 w-full bg-white p-8">
-            <div className="flex items-center justify-center w-full">
+        <div className="fixed top-0 w-full bg-neutral-100 p-6">
+            <div className="flex items-center justify-between w-full">
                 <h1 className="heading-1">
                     <span className="text-blue-400">Guardian</span>
                     <span className="text-blue-950">Filtered</span>
                 </h1>
-                <form className="w-full max-w-xl">
-                    <input className="searchbar w-full" name="query" autocomplete="off" placeholder="Scour the archives..."/>   
-                </form>
+                <div className="">
+                    <form className="">
+                        <input className="searchbar" name="query" autocomplete="off" placeholder="Scour the archives..."/>
+                    </form>
+                </div>
                 <div className="flex justify-center items-center ">
                     <span><h2 className="heading-2">Filter by category</h2></span>
                     <span className="hover:scale-105 hover:opacity-100 opacity-60 transition-all cursor-pointer ease-linear"><NavBarIcon icon={<FaFilter size="16" />} /></span>
