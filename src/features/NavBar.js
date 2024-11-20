721e48083217ad380538a96495675079e60ae8ff
Nick Saunders <njsaunders97@gmail.com>
2024-11-20 12:24:24 +0100

    Fixed navbar

--- src/features/NavBar.js
+++ src/features/NavBar.js
@@ -3,80 +3,54 @@ import { FaFilter } from 'react-icons/fa';
 
 function NavBar() {
     return (
-        <div className=" fixed w-full top-0 left-0 right-0
-                        bg-neutral-100"> {/* NavBar */}
-            {/* DESKTOP */}
-            <div className="flex flex-wrap justify-around m-auto items-center md:justify-around
-                            py-4"> {/* NavBar Container */}
-                <div className="heading-1 hidden flex-shrink lg:flex">
-                    <span className="heading-1 text-blue-950">Guardian</span>
-                    <span className="heading-1 text-blue-400">Filtered</span>
-                </div>
-                <div className="hidden flex-shrink lg:flex pr-80 pl-60">
-                    <form className="">
-                        <input
-                        className="searchbar text-center px-10 py-2"
-                        name="query"
-                        autocomplete="off"
-                        placeholder="Scour the archives..."
-                        >
-                        </input>
-                    </form>
-                </div>
-                <div className="items-center hidden lg:flex">
-                    <span className="heading-2 mr-2">Filter by category</span>
-                    <span className="hover:scale-105 hover:opacity-100 opacity-60 transition-all cursor-pointer ease-linear"><NavBarIcon icon={<FaFilter size="12" />} /></span>
-                </div>
-
-                {/* TABLET */}
-                <div className="flex flex-grow items-center justify-around list-none lg:hidden md:flex sm:hidden">
-                    <li className="px-2">
-                        <h1 className="heading-1-phone">
+        <div className="fixed w-full top-0 left-0 right-0"> {/* NavBar Container */}
+            <header className="pointer-events-auto border-solid border-0 border-b 
+                                px-4
+                                border-neutral-100 bg-neutral-100">
+                <nav className="flex items-center
+                                h-[75px]
+                                ">
+                    <div className="flex items-center
+                                    
+                                    ">
+                        <h1 className="heading-1 hidden lg:flex">
+                            <span className="text-blue-950">Guardian</span>
+                            <span className="text-blue-400">Filtered</span>
+                        </h1>
+                        <h1 className="heading-1-tablet lg:hidden">
                             <span className="text-blue-950">G</span>
                             <span className="text-blue-400">F</span>
                         </h1>
-                    </li>
-                    <li className="flex justify-center px-2">
-                        <form className="">
-                            <input
-                            className="searchbar-phone text-center px-20 py-2"
-                            name="query"
-                            autocomplete="off"
-                            placeholder="Tablet"
-                            >
-                            </input>
-                        </form>
-                    </li>
-                    <li className="px-2">
-                        <span className="hover:scale-105 hover:opacity-100 opacity-60 transition-all cursor-pointer ease-linear"><NavBarIcon icon={<FaFilter size="14" />} /></span>
-                    </li>
-                </div>
-                
-
-                {/* PHONE */}
-                <div className="flex flex-grow items-center justify-around list-none md:hidden">
-                    <li className="px-2">
-                        <h1 className="heading-1-phone">
-                            <span className="text-blue-950">G</span>
-                            <span className="text-blue-400">F</span>
-                        </h1>
-                    </li>
-                    <li className="flex justify-center px-2">
-                        <form className="">
-                            <input
-                            className="searchbar-phone text-center px-4 py-2"
-                            name="query"
-                            autocomplete="off"
-                            placeholder="Phone"
-                            >
-                            </input>
-                        </form>
-                    </li>
-                    <li className="px-2">
-                        <span className="hover:scale-105 hover:opacity-100 opacity-60 transition-all cursor-pointer ease-linear"><NavBarIcon icon={<FaFilter size="10" />} /></span>
-                    </li>
-                </div>
-            </div>
+                    </div>
+                    <div className="flex items-center justify-stretch flex-1
+                                    h-[40px] lg:-ml-7 md:ml-[20px]
+                                    ">
+                        <div className="flex w-full
+                                        ">
+                            <div className="flex w-full justify-center md:mx-auto">
+                                <form className="flex">
+                                    <input
+                                    className="searchbar text-center lg:w-96 md:w-60 py-2"
+                                    name="query"
+                                    autocomplete="off"
+                                    placeholder="Scour the archives..."
+                                    >
+                                    </input>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center
+                                    md:pl-6 pl-3">
+                        <div className="flex items-center 
+                                        h-[40px]
+                                        ">
+                            <span className="heading-2 hidden lg:flex lg:mr-2">Filter by category</span>
+                            <span className="md:mr-2 hover:scale-105 hover:opacity-100 opacity-60 transition-all cursor-pointer ease-linear"><NavBarIcon icon={<FaFilter size="11" />} /></span>
+                        </div>
+                    </div>
+                </nav>
+            </header>  
         </div>
     )
 };
